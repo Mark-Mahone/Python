addbce2ef7f598c34dfa43851769e1f41f19152b
Admin <Admin@PF2CT76W-SHJ.inc.bytedance.com>
2021-03-17 18:13:52 +0800

    +

--- Function.py
+++ Function.py
@@ -38,14 +38,30 @@
 
 # print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower))
 
-L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
-def paiming(x):
-    return x[0]
+# L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+# def paiming(x):
+#     return x[0]
 
-def fenshu(x):
-    return -x[1]
+# def fenshu(x):
+#     return -x[1]
 
-L2 = sorted(L, key=paiming)
-print(L2)
-L3 = sorted(L, key=fenshu)
-print(L3)
+# L2 = sorted(L, key=paiming)
+# print(L2)
+# L3 = sorted(L, key=fenshu)
+# print(L3)
+
+def createCounter():
+    x = 0
+    def counter():
+        nonlocal x
+        x = x + 1
+        return x
+    return counter
+
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+    print('测试通过!')
+else:
+    print('测试失败!')
