cee8018ce441b42d50a8f021cf013ca9f42a7199
Admin <Admin@PF2CT76W-SHJ.inc.bytedance.com>
2021-03-18 18:41:55 +0800

    +

--- Function.py
+++ Function.py
@@ -1,3 +1,4 @@
+import time, functools
 # def odd(x):
 #     return x % 2 == 1
 # print(list(filter(odd, [1,2,3,4,5,6,7,8,9])))
@@ -50,18 +51,77 @@
 # L3 = sorted(L, key=fenshu)
 # print(L3)
 
-def createCounter():
-    x = 0
-    def counter():
-        nonlocal x
-        x = x + 1
-        return x
-    return counter
+# def createCounter():
+#     x = 0
+#     def counter():
+#         nonlocal x
+#         x = x + 1
+#         return x
+#     return counter
 
-counterA = createCounter()
-print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
-counterB = createCounter()
-if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
-    print('测试通过!')
-else:
-    print('测试失败!')
+# counterA = createCounter()
+# print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+# counterB = createCounter()
+# if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+#     print('测试通过!')
+# else:
+#     print('测试失败!')
+
+# def nth_power(exponent):
+#     def exponent_of(base):
+#         return base ** exponent
+#     return exponent_of # 返回值是 exponent_of 函数
+# square = nth_power(2) # 计算一个数的平方
+# cube = nth_power(3) # 计算一个数的立方
+# print(square(2))  # 计算 2 的平方
+# print(cube(2)) # 计算 2 的立方
+
+# def build(x, y):
+#     return lambda z: x * x + y * y + z
+
+# print(build(2,3)(5))
+
+# def is_odd(n):
+#     return n % 2 == 1
+
+# L = list(filter(is_odd, range(1, 20)))
+# print(L)
+# print(list(filter((lambda x: x % 2 == 1), range(1, 20))))
+
+# def log(func):
+#     def wrapper(*args, **kw):
+#         print('call %s():' % func.__name__)
+#         return func(*args, **kw)
+#     return wrapper
+# @log
+# def now():
+#     print('2015-3-25')
+# print(now())
+
+# def log(func):
+#     def wrapper(*args, **kw):
+#         print('call %s()' % func.__name__)
+#         return func(*args, **kw)
+#     return wrapper
+
+# @log
+# def now():
+#     n = 1
+#     print('2015-3-25')
+
+# now()
+# print(now.__name__)
+
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print("%s %s()" % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+@log(123)
+def now():
+    print('2021-3-5')
+now()
+print(now.__name__)
+
